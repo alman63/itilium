d7237ffa360fd21db6506c08c7e2f945c24b5461
Alman63 <mantulenko83@mail.ru>
2025-04-02 08:56:11 +0400

    fix: add fetch for domain

---
+++ index.js
@@ -0,0 +1,15 @@
+// const credentials = btoa("glpi2:Sseu2024");
+
+// fetch("http://10.10.80.9:636", {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `Basic ${credentials}`,
+//   },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
